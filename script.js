832bfa83d66d899df48b7830bd61b7d8cd2fae46
Vishal Kumar <125990518+vishalqalandari903@users.noreply.github.com>
2023-08-19 15:25:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let menuList = document.querySelector("#menuList");
+function togglemenu() {
+  menuList.classList.toggle("active");
+}
+// console.log(window);
+// document.addEventListener("click", function (e) {
+//   // if (menuList.classList.contains("active")) {
+//   if (e.target.id !== "menuList" && e.target.id !== "menuIcon") {
+//     // menuList.classList.remove("active");
+//     console.log("inside");
+//   }
+//   // }
+// });
